8ea8dab1254b82bac01a844ba1fb2085225ba1bb
김영훈 <eth220421@gmail.com>
2024-07-21 18:46:22 +0900

    24.07.21
    Header, SideBar, Footer, MainPage-Banner

---
+++ src/components/footer/events.ts
@@ -0,0 +1,7 @@
+export const useOwnerInfo = () => {
+  const ownerName = '김유성';
+  const ownerEmail = 'kimys5278@gmail.com';
+  const ownerTel = '010-5278-9082';
+
+  return { ownerName, ownerEmail, ownerTel };
+};
---
+++ src/components/nav/styles.ts
@@ -0,0 +1,70 @@
+import styled, { keyframes } from 'styled-components';
+
+const fadeIn = keyframes`
+    from {
+        transform: translateX(0%);
+    }
+    to {
+        transform: translateX(100%);
+    }
+`;
+
+const fadeOut = keyframes`
+    from {
+        transform: translateX(100%);
+    }
+    to {
+        transform: translateX(0%);
+    }
+`;
+
+export const NavContainer = styled.div<{ $isClosing: boolean }>`
+  width: 100%;
+  height: 100%;
+
+  padding-top: 2rem;
+
+  animation: ${({ $isClosing }) => ($isClosing ? fadeIn : fadeOut)} 0.4s ease-in-out;
+`;
+
+export const NavContent = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: start;
+
+  padding: 1.5rem 3rem;
+  font-weight: 600;
+  font-size: 20px;
+  color: #000;
+`;
+
+export const NavDetails = styled.details``;
+
+export const NavSummary = styled.summary`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+
+  padding: 1.5rem 3rem;
+  font-weight: 600;
+  font-size: 20px;
+  color: #000;
+`;
+
+export const NavList = styled.ul`
+  display: flex;
+  align-items: center;
+  justify-content: start;
+
+  padding: 0.5rem 3.5rem;
+  font-size: 16px;
+  font-weight: 500;
+  color: #333;
+`;
+
+export const StateBtn = styled.div`
+  padding: 1rem 3rem;
+  font-size: 13px;
+  font-weight: 500;
+  color: #5d5d5d;
+`;
